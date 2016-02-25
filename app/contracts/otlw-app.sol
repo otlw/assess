3ffbfe8b253d786ce58e449ecb759222428ae643
lordskh <saianeeshkh@gmail.com>
2016-02-25 19:36:45 +0400

    Made system for adding tags and parents for tags. Contemplated murder suicide with Jared.

--- app/contracts/otlw-app.sol
+++ app/contracts/otlw-app.sol
@@ -1,6 +1,6 @@
-contract otlwApp
+contract Master
 {
-  function otlwApp()
+  function Master()
   {
 
   }
@@ -8,18 +8,52 @@ contract otlwApp
   mapping (address => string) tagName;
   mapping (string => address) tagAddress;
   mapping (address => string[]) acheivements;
-  function addTag(string name) returns(uint)
+  function addTag(string name, address[] parentList) returns(uint)
   {
     uint response = 0;
     if(tokenBalance[msg.sender] < 1)
+    {
       response += 1;
+    }
     if(tagAddress[name] == 0)
+    {
       response += 10;
+    }
     if(response==0)
     {
-      //Put the code to actually create a new tag contract here
-      //Need to create a general purpose tag contract to be used as a template for this
+      Tag newTag;
+      address newTagAddress = address(newTag);
+      tagName[newTagAddress] = name;
+      tagAddress[name] = newTagAddress;
+      for(uint i=0; i<= parentList.length; i++)
+      {
+        if(parentList[i]==0)
+        {
+          response += 100;
+        }
+        if(response==0)
+        {
+          newTag.addParent(parentList[i]);
+        }
+      }
     }
     return response;
+  }
+  function getTagName(address parentAddress) returns(string)
+  {
+    return tagName[parentAddress];
+  }
+}
+
+contract Tag
+{
+  address[] parents;
+  function Tag()
+  {
+
+  }
+  function addParent(address parent)
+  {
+    parents.push(parent);
   }
 }
