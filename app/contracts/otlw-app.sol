cc3a89d4a24b280c35c5dbafec616dbc40662200
lordskh <saianeeshkh@gmail.com>
2016-02-25 10:33:08 +0400

    Changed the way that errors are reported so more robust error messages can be produced. Also, put in skeleton code for the creation of new tag contract.

--- app/contracts/otlw-app.sol
+++ app/contracts/otlw-app.sol
@@ -1,18 +1,25 @@
 contract otlwApp
 {
-        function otlwApp()
-        {
-        }
-        mapping (address => uint) tokenBalance;
-        mapping (address => string) tagName;
-        mapping (string => address) tagAddress;
-        mapping (address => string[]) acheivements;
-        function addTag(string name) returns(uint response)
-        {
-                if(tokenBalance[msg.sender] < 1)
-                        return 1;
-                if(tagAddress[name] == 0)
-                        return 2;
-                
-        }
+  function otlwApp()
+  {
+
+  }
+  mapping (address => uint) tokenBalance;
+  mapping (address => string) tagName;
+  mapping (string => address) tagAddress;
+  mapping (address => string[]) acheivements;
+  function addTag(string name) returns(uint)
+  {
+    uint responce = 0;
+    if(tokenBalance[msg.sender] < 1)
+      responce += 1;
+    if(tagAddress[name] == 0)
+      responce += 10;
+    if(responce==0)
+    {
+      //Put the code to actually create a new tag contract here
+      //Need to create a general purpose tag contract to be used as a template for this
+    }
+    return responce;  
+  }
 }
