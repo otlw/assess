6b4589662b458466bb290bf55cc23f900507e3ee
lordskh <saianeeshkh@gmail.com>
2016-02-25 15:41:10 +0400

    Fixed typos

--- app/contracts/otlw-app.sol
+++ app/contracts/otlw-app.sol
@@ -10,16 +10,16 @@ contract otlwApp
   mapping (address => string[]) acheivements;
   function addTag(string name) returns(uint)
   {
-    uint responce = 0;
+    uint response = 0;
     if(tokenBalance[msg.sender] < 1)
-      responce += 1;
+      response += 1;
     if(tagAddress[name] == 0)
-      responce += 10;
-    if(responce==0)
+      response += 10;
+    if(response==0)
     {
       //Put the code to actually create a new tag contract here
       //Need to create a general purpose tag contract to be used as a template for this
     }
-    return responce;  
+    return response;
   }
 }
