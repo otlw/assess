c4cee45ae804555d5c1934bc8639e4e01e875d70
lordskh <saianeeshkh@gmail.com>
2016-04-28 15:55:49 +0400

    Made functions to get a random number. Needs to be tested and then properly implemented into main codebase.

--- contracts/sortaRandom.sol
+++ contracts/sortaRandom.sol
@@ -1,14 +1,20 @@
 import "api.sol";
 
-
-contract RandomNumbers is usingOraclize {
-
-
-  function sortaRandomGen() returns(uint) { //function using oraclize to get a random number from wolfram alpha
-
+contract RandomNumbers is usingOraclize
+{
+  function getSeedNumber() returns(uint)
+  {
     oraclize_setNetwork(networkID_mainnet);
-
     return uint(oraclize_query("WolframAlpha", "random number between 0 and 100"));
+  }
 
+  function getRandom(uint max) returns(uint)
+  {
+    return ((getSeedNumber()/100)*max);
+  }
+
+  function getRandom(uint min, uint max) returns(uint)
+  {
+    return ((getSeedNumber()/100)*(max-min)+min);
   }
 }
