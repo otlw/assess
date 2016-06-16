420e02145835cad40c296936d207bf702de57574
lordskh <saianeeshkh@gmail.com>
2016-06-16 23:36:15 +0400

    FIxed the range shifter on the getRandom(uint seed, uint min, uint max) function

--- lib/random.sol
+++ lib/random.sol
@@ -7,6 +7,6 @@ contract Random
 
   function getRandom(uint seed, uint min, uint max) constant returns (uint randomNumber) //Based on the original function written by github user alexvandesande
   {
-    return(uint(sha3(block.blockhash(block.number-1), seed))%(max+min) + min);
+    return(uint(sha3(block.blockhash(block.number-1), seed))%(max-min) + min);
   }
 }
