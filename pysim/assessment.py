7b8f7452157936eb33030523783ab8f1c55f5050
djudjuu <julfaber@gmail.com>
2017-06-14 15:18:18 +0200

    median distance, edge cases

--- pysim/assessment.py
+++ pysim/assessment.py
@@ -3,9 +3,14 @@ import sys
 
 # simulates the scoring process of an assessment. Can be used to
 # a) to test different clustering functions with testCalculateResults()
+#    -> usage: comment/uncomment in main() and execute without arguments
+#
 # b) to compute the avgScore and the winningAsssossors for a given series of scores
-#   (passed as a comma separated string to the command line, e.g.
+#   usage: pass scores as a comma separated string to the command line, e.g.
 #  > python assessment.py 0,1,0,0,1
+
+# c) some edge cases with more complicated scores that are interesting to look at
+# usage: umcomment main() and set edge=True
 
 # ============== scoring and clustering functions ===================
 
@@ -36,7 +41,7 @@ def clusterSOL(scores, distanceFunction=MAD, verbose=False):
     clusters = [[] for i in range(n)]
     for l in range(n):
         for m in range(n):
-            if scores[l] - scores[m] <= dist:  # this should be abs()?
+            if scores[l] - scores[m] <= dist:  # shouldnt this be abs()?->no still works? why?
                 clusters[l].append(scores[m])
     if verbose:
         printClusters(clusters, dist, distanceFunction == MAD)
@@ -71,7 +76,7 @@ def calculateResult(
     avgScore = np.mean(winnerCluster)
     return avgScore, winnerAssessors
 
-# ============== a) TESTS ====================
+# ============== test-functions ====================
 
 
 def testMAD():
@@ -95,6 +100,7 @@ def testCalculateResults(
     # (scoringArray, expected avgScore, expected winningAssessors)
     cases = [
         ([1, 1], 1, [0, 1]),
+        ([1, 1, -1], 1, [0, 1]),
         ([0, 1, 1, 1], 1, [1, 2, 3]),
         ([1, 1, 1, 0], 1, [0, 1, 2]),
         #        ([1,1,0,0,], ??, [?,?]), #TODO what happens at a  draw?
@@ -112,23 +118,66 @@ def testCalculateResults(
     return True
 
 
-# ============== b) read scores from input ===================
-# enter scores as string
-if len(sys.argv) > 1:
-    print "reading from CL..."
-    scores = np.asarray([float(s) for s in sys.argv[1].split(',')])
-    print scores
-    avgScore, winnerAssessors = calculateResult(
-        scores, cluster1, MAD, verbose=True
-    )
-    print "winningScores: ", scores[winnerAssessors]
-    print "avgScore: ", avgScore, "\nwinning Assessors: ",  winnerAssessors
-else:
-    print "testing the implemented functions..."
-    testMAD()
-    verbose = True
-    testCalculateResults(clusterSOL, MAD, verbose=verbose)
-    # testCalculateResults(cluster1, MAD, verbose=verbose)
+# ======== MAIN ============
+def main():
+    if len(sys.argv) > 1:
+        # ============== b) read scores from input ===================
+        print "reading from CL..."
+        distanceFunction = MAD
+        clusterFunction = clusterSOL
+        scores = np.asarray([float(s) for s in sys.argv[1].split(',')])
+        try:
+            if sys.argv[2] == "mad":
+                distanceFunction = MAD
+            if sys.argv[2] == "mmd":
+                distanceFunction = MMD
+            if sys.argv[3] == "1":
+                clusterFunction = cluster1
+            if sys.argv[2] == "1":
+                clusterFunction = cluster1
+        except:
+            print ""
+        print "cluster-Function: ", clusterFunction
+        print "scores: ", scores
+        avgScore, winnerAssessors = calculateResult(
+            scores, clusterFunction, distanceFunction, verbose=True
+        )
+        print "winningScores: ", scores[winnerAssessors]
+        print "avgScore: ", avgScore, "\nwinning Assessors: ",  winnerAssessors
+    else:
+        # ============== b) test clustering functions ===================
+        print "testing the implemented functions..."
+        testMAD()
+        verbose = True
+        testCalculateResults(clusterSOL, MAD, verbose=verbose)
+
+        # testCalculateResults(cluster1, MAD, verbose=verbose)
+# main()
 
 
 # ================= edge cases ========================
+# here I enter tricky cases that might break the system. one assessor going crazy high.
+# to see the effect on the score and the cluster
+edge = True  # False
+
+# compare-flag: true -> runs both clusterSOL and cluster1
+# and prints the results next to each other
+verbose = True
+if edge:
+    print "Looking at edge cases..."
+    cases = [
+        # this one looks bad for clusterSOL and good for cluster1?
+        np.asarray([1, 1, 1, 1, 0, 1000]),
+        # this one will have many three winning clusters, each with 3 scores -> DRAW?
+        np.asarray([1, 2, 3, 4, 5]),
+        # how to deal with a draw
+        np.asarray([1, 1, 1, 2, 2, 2])
+    ]
+    for scores in cases:
+        print "===== ", scores, " =========="
+        avgScore, winnerAssessors = calculateResult(
+            scores, cluster1, MMD, verbose  # <change here betwen clusterSOL/1
+        )
+        print "winningScores: ", scores[winnerAssessors]
+        print "avgScore: ", avgScore
+        print "winning Assessors: ",  winnerAssessors
