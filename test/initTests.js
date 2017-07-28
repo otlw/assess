7a9fc15576f26dbb75efb9e5453517d51c28d2b4
Jared Pereira <jared@otlw.co>
2017-07-28 21:17:27 +0000

    Refactor tests to use async/await

--- test/initTests.js
+++ test/initTests.js
@@ -1,101 +1,69 @@
 var ConceptRegistry = artifacts.require("ConceptRegistry");
 var UserRegistry = artifacts.require("UserRegistry");
 var Concept = artifacts.require("Concept");
-var UserRegistryInstance;
+
+var conReg
+var userReg
 
 contract("ConceptRegistry", function() {
     var mewAddress;
     var createdConceptAddress;
-    var ConceptRegistryInstance
-    it("should have created the mew contract", function(){
-        ConceptRegistry.deployed().then(function(instance) {
-            ConceptRegistryInstance = instance
-            return ConceptRegistryInstance.mewAddress.call();
-        }).then(function(address){
-            mewAddress = address
-            return ConceptRegistryInstance.conceptExists(mewAddress)
-        }).then(function(exists){
-            assert.isTrue(exists, "mew doesn't exist")
-        })
-    })
-    it("should set mew as the parent when not specified", function() {
-        return ConceptRegistry.deployed().then(function(instance) {
-            return instance.makeConcept([], [500], 60*60*24, "")
-        }).then(function(result){
-            createdConceptAddress = result.logs[0].args["_concept"]
-            return Concept.at(mewAddress)
-        }).then(function(mewInstance) {
-            return Concept.at(createdConceptAddress)
-        }).then(function(conceptInstance){
-            return conceptInstance.parents.call(0)
-        }).then(function(parentAddress){
-            assert.equal(parentAddress, mewAddress)
-        })
-    });
-    it("should set the parents if specified", function() {
-        var childConceptAddress
 
-        return ConceptRegistry.deployed().then(function(instance) {
-            //create a 2nd concept to be a child of mew
-            parentAddress = createdConceptAddress
-            return instance.makeConcept([parentAddress], [500], 60*60*24, "")
-        }).then(function(result){ //catch transaction result
-            //fetch the address of the created contract from the Event-log
-            childConceptAddress = result.logs[0].args["_concept"]
-            childConcept = Concept.at(childConceptAddress); //create abstraction for the created Concept
-            //check that the parent is linked to the child-concept
-            return childConcept.parents.call(0)
-        }).then(function(parentAddressFromChild){
-            assert.equal(parentAddressFromChild, parentAddress, "Child concept does not know its parent")
-        })
+    it("should have created the mew contract", async () => {
+        conReg = await ConceptRegistry.deployed()
+        mewAddress = await conReg.mewAddress.call()
+
+        assert.isTrue(await conReg.conceptExists.call(mewAddress), "mew doesn't exist")
+    })
+    it("should set mew as the parent when not specified", async () => {
+        let txReciept = await conReg.makeConcept([], [500], 60*60*24, "")
+        createdConceptAddress = txReciept.logs[0].args["_concept"]
+
+        let createdConcept = await Concept.at(createdConceptAddress)
+
+        assert.equal( await createdConcept.parents.call(0), mewAddress)
     });
-    it("should throw if parent doesn't exist", function(){
-        return ConceptRegistry.deployed().then(function(instance) {
-            return instance.makeConcept(["0x123"], [500], 60*60*24, "")
-        }).then(function (result) {
-            assert(false, "transaction should fail")
-        }).catch(function(e){
+
+    it("should set the parents if specified", async () => {
+        let txReceipt = await conReg.makeConcept([createdConceptAddress], [500], 60*60*24, "")
+
+        let childConcept = await Concept.at(txReceipt.logs[0].args["_concept"])
+        assert.equal(await childConcept.parents.call(0), createdConceptAddress, "Child concept doesn't know supplied parent")
+    });
+
+    it("should throw if parent doesn't exist", async () => {
+        try {
+            await conReg.makeConcept(["0x123"], [500], 60*60*24, "")
+        } catch (e) {
             if(e.toString().indexOf('invalid opcode') > 0) {
                 assert(true)
             }
             else {
                 assert(false, e.toString())
             }
-        })
+        }
     })
 })
 
 contract("Initial User", function(accounts){ 
     it("should have a balance of 10000000000", async () => {
-        userRegistryInstance = await UserRegistry.deployed()
-        balance = await userRegistryInstance.balances.call(accounts[0])
+        userReg = await UserRegistry.deployed()
+        balance = await userReg.balances.call(accounts[0])
         assert.equal(balance.toNumber(),10000000000, "User0 does not have 10 billion Ahas")
-        balance = await userRegistryInstance.balances.call(accounts[4])
+        balance = await userReg.balances.call(accounts[4])
         assert.equal(balance.toNumber(),10000000000, "User4 does not have 10 billion Ahas")
     });
 })
 
 contract("token transfers", function(accounts) {
-    it("Should modify balances correctly", function(){
-        var account1InitialBalance;
-        var account2InitialBalance;
+    it("Should modify balances correctly", async () => {
+        var account1InitialBalance = await userReg.balances.call(accounts[0]);
+        var account2InitialBalance = await userReg.balances.call(accounts[1]);
         var amount = 50;
-        return UserRegistry.deployed().then(function(instance){
-            UserRegistryInstance = instance
-            return UserRegistryInstance.balances.call(accounts[0])
-        }).then(function(balance){
-            account1InitialBalance = balance
-            return UserRegistryInstance.balances.call(accounts[1])
-        }).then(function(balance) {
-            account2InitialBalance = balance
-            return UserRegistryInstance.transfer(accounts[1], amount, {from: accounts[0]})
-        }).then(function() {
-            return UserRegistryInstance.balances.call(accounts[0])
-        }).then(function(balance) {
-            assert.equal(account1InitialBalance - balance.toNumber(), amount, "User1 didn't lose tokens")
-            return UserRegistryInstance.balances.call(accounts[1])
-        }).then(function(balance) {
-            assert.equal(balance.toNumber() - account2InitialBalance, amount, "User2 didn't gain tokens")
-        })
+
+        await userReg.transfer(accounts[1], amount, {from: accounts[0]})
+
+        assert.equal(account1InitialBalance - await userReg.balances.call(accounts[0]), amount)
+        assert.equal(await userReg.balances.call(accounts[1]) - account1InitialBalance, amount)
    })
 })
