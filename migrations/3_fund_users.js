6b125db937b3d0c4cb6c6c27c6a8b961fa23c9de
Antoine Estienne <estienne.antoine@gmail.com>
2018-03-27 21:14:59 +0000

    removed commented lines

--- migrations/3_fund_users.js
+++ migrations/3_fund_users.js
@@ -1,18 +1,6 @@
 var FathomToken = artifacts.require("./FathomToken.sol");
 var accounts, setup;
 
-// try {
-//     //NOTE: this should only be used when deploying to the rinkeby-testnet.
-//     // For development please use the accounts from the web3-object
-//     setup = require("./../initialMembers.json")
-//     console.log("using provided list of initial members.")
-//     accounts = setup.accounts
-// }
-// catch(e) {
-//     var nInitialUsersWithFunds = 9
-//     accounts = web3.eth.accounts.slice(0,nInitialUsersWithFunds)
-// }
-
 module.exports = function(deployer) {
     //choose accounts depending on network
   if (deployer.network==="development"){
