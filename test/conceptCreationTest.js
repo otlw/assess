912936f4d3f919fb42fdf6785810d623474eb879
“joelamouche” <estienne.antoine@gmail.com>
2018-04-03 18:42:03 +0200

    added a test for concept creations to understand their behavior

---
+++ test/conceptCreationTest.js
@@ -0,0 +1,46 @@
+var ConceptRegistry = artifacts.require("ConceptRegistry");
+var FathomToken = artifacts.require("FathomToken");
+var Concept = artifacts.require("Concept");
+
+var conReg
+var aha
+
+contract("ConceptRegistry", function(accounts) {
+    var mewAddress;
+    var createdConceptAddress;
+
+    it("should have created the mew contract", async () => {
+        conReg = await ConceptRegistry.deployed()
+        mewAddress = await conReg.mewAddress.call()
+
+        assert.isTrue(await conReg.conceptExists.call(mewAddress), "mew doesn't exist")
+    })
+    it("should be able to create a first concept from the MEW", async () => {
+        let txReciept = await conReg.makeConcept([mewAddress], [500], 60*60*24, "",accounts[0])
+        createdConceptAddress = txReciept.logs[0].args["_concept"]
+
+        let createdConcept = await Concept.at(createdConceptAddress)
+        assert.isTrue(await conReg.conceptExists.call(createdConceptAddress), "concept1 doesn't exist")
+    });
+    it("should not be able to create a second concept from the MEW with same data", async () => {
+        let txRecieptS = await conReg.makeConcept([mewAddress], [500], 60*60*24, "",accounts[0])
+        createdConceptAddressS = txRecieptS.logs[0].args["_concept"]
+
+        let createdConceptS = await Concept.at(createdConceptAddressS)
+        assert.isNotTrue(await conReg.conceptExists.call(createdConceptAddressS), "a new concept should NOT have been created")
+    });
+    it("should be able to create another concept from the MEW", async () => {
+        let txReciept2 = await conReg.makeConcept([mewAddress], [500], 60*60*24, "okok",accounts[0])
+        createdConceptAddress2 = txReciept2.logs[0].args["_concept"]
+
+        let createdConcept2 = await Concept.at(createdConceptAddress2)
+
+        assert.isTrue(await conReg.conceptExists.call(createdConceptAddress2), "concept2 doesn't exist")
+    });
+    it("shouldn't be the same address for both concepts", async () => {
+        console.log("createdConceptAddress : "+createdConceptAddress)
+        console.log("createdConceptAddress2 : "+createdConceptAddress2)
+
+        assert.notEqual(createdConceptAddress,createdConceptAddress2, "concept1 and concept2 are the same")
+    });
+})
