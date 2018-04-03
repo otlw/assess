5299f27917445d9662a63f1ff4fdcb9709c6d9fe
“joelamouche” <estienne.antoine@gmail.com>
2018-04-03 20:03:28 +0200

    finished test to allow two concepts creations for same data

--- test/conceptCreationTest.js
+++ test/conceptCreationTest.js
@@ -22,14 +22,20 @@ contract("ConceptRegistry", function(accounts) {
         let createdConcept = await Concept.at(createdConceptAddress)
         assert.isTrue(await conReg.conceptExists.call(createdConceptAddress), "concept1 doesn't exist")
     });
-    it("should not be able to create a second concept from the MEW with same data", async () => {
+    it("should be able to create a second concept from the MEW with same data", async () => {
         let txRecieptS = await conReg.makeConcept([mewAddress], [500], 60*60*24, "",accounts[0])
         createdConceptAddressS = txRecieptS.logs[0].args["_concept"]
 
         let createdConceptS = await Concept.at(createdConceptAddressS)
-        assert.isNotTrue(await conReg.conceptExists.call(createdConceptAddressS), "a new concept should NOT have been created")
+        assert.isTrue(await conReg.conceptExists.call(createdConceptAddressS), "a new concept should have been created")
     });
-    it("should be able to create another concept from the MEW", async () => {
+    it("shouldn't be the same address for both concepts (same data)", async () => {
+        console.log("createdConceptAddress : "+createdConceptAddress)
+        console.log("createdConceptAddressS : "+createdConceptAddressS)
+
+        assert.notEqual(createdConceptAddress,createdConceptAddressS, "concept1 and concept1(same data) are the same")
+    });
+    it("should be able to create another concept from the MEW with different data", async () => {
         let txReciept2 = await conReg.makeConcept([mewAddress], [500], 60*60*24, "okok",accounts[0])
         createdConceptAddress2 = txReciept2.logs[0].args["_concept"]
 
@@ -37,7 +43,7 @@ contract("ConceptRegistry", function(accounts) {
 
         assert.isTrue(await conReg.conceptExists.call(createdConceptAddress2), "concept2 doesn't exist")
     });
-    it("shouldn't be the same address for both concepts", async () => {
+    it("shouldn't be the same address for both concepts (different data)", async () => {
         console.log("createdConceptAddress : "+createdConceptAddress)
         console.log("createdConceptAddress2 : "+createdConceptAddress2)
 
