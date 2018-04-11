5ddf22e85beea63f783c5cf492d42651928b706a
“joelamouche” <estienne.antoine@gmail.com>
2018-04-11 13:36:17 +0200

    added style on assmnt creation component

--- app/components/AssessmentCreationBar.js
+++ app/components/AssessmentCreationBar.js
@@ -2,7 +2,27 @@ import { Component } from 'react'
 import Dropdown from "./Dropdown"
 var h = require('react-hyperscript')
 
-export class ConceptList extends Component {
+const assessmentCreationStyle={
+  frame:{
+    marginTop:"1em",
+    padding:"0.3em",
+    textAlign:"left",
+    border:"0.5px solid lightgrey",
+    borderRadius:"0.3em"
+  },
+  fieldName:{
+    fontWeight:"bold",
+    display:"inline-block"
+  },
+  dropdown:{
+    marginLeft:"1em",
+    border:"1px solid lightblue",
+    padding:"0.2em",
+    display:"inline-block"
+  }
+}
+
+export class AssessmentCreationBar extends Component {
   constructor(props) {
     super(props);
     this.state={
@@ -15,8 +35,13 @@ export class ConceptList extends Component {
   }
 
   render () {
-    return h(Dropdown,{list:this.props.conceptAddressList,selectedID:this.state.selectedConceptKey,set:this.setConceptKey.bind(this)})
+    return h("div",{style:assessmentCreationStyle.frame},[
+      h("div",{style:assessmentCreationStyle.fieldName},"Select Concept"),
+      h("div",{style:assessmentCreationStyle.dropdown},
+        h(Dropdown,{list:this.props.conceptAddressList,selectedID:this.state.selectedConceptKey,set:this.setConceptKey.bind(this)})
+      )
+    ])
   }
 }
 
-export default ConceptList
+export default AssessmentCreationBar
