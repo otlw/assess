646da3265936d89a03d119adac21b3b5cc2304c5
“joelamouche” <estienne.antoine@gmail.com>
2018-04-26 11:26:28 -0400

    removed dummy lines from assessorList

--- app/components/AssessmentView/AssessorList.js
+++ app/components/AssessmentView/AssessorList.js
@@ -5,8 +5,6 @@ var h = require('react-hyperscript')
 // component to display all assessors
 export class AssessorList extends Component {
   render () {
-    this.props.assessors.push({address: 'testAssessor', stage: 1})
-    this.props.assessors.push({address: '0xf2a2E600Eb309A5d8A17C18756F65608bD5ce5Db', stage: 2})
     return h('div',
       this.props.assessors.map((assessor, k) => {
         return h(AssessorStatusBox, {
