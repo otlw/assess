0297c86d7360c8b3aac7e3da5604338280c8ff45
Jared Pereira <jared@otlw.co>
2018-05-29 11:11:06 -0400

    move selectedTab in Dashboard to redux state
    This is so that the selected tab doesn't reset every time the componenent
    remounts.

--- app/components/Dashboard/FilterView/index.js
+++ app/components/Dashboard/FilterView/index.js
@@ -1,11 +1,17 @@
 import { connect } from 'react-redux'
 import FilterView from './FilterView'
+import {setDashboardTab} from '../../../actions/navigationActions.js'
 
 const mapStateToProps = state => {
   return {
     assessments: state.assessments,
-    userAddress: state.ethereum.userAddress
+    userAddress: state.ethereum.userAddress,
+    selectedTab: state.navigation.dashboardTab
   }
 }
 
-export default connect(mapStateToProps)(FilterView)
+const mapDispatchToProps = {
+  setDashboardTab
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FilterView)
