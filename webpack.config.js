05fee642cc9e363ead5d990d10e400c56faf1981
“joelamouche” <estienne.antoine@gmail.com>
2018-06-18 16:19:03 -0400

    more experimenting with webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,12 @@ module.exports = {
       {
          test: /\.(png|svg|jpg|gif)$/,
          use: [
-           'file-loader'
+           {loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'assets/'
+            }
+          }
          ]
       }
     ]
