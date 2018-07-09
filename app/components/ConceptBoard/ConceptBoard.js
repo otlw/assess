ff068e322f393fcd9a481b02226a2308246cb344
“joelamouche” <estienne.antoine@gmail.com>
2018-07-09 12:55:11 -0400

    finished first step of concept creation and moved it to a separate component

--- app/components/ConceptBoard/ConceptBoard.js
+++ app/components/ConceptBoard/ConceptBoard.js
@@ -1,7 +1,9 @@
 import { Component } from 'react'
-import ConceptList from './ConceptList'
 import styled from 'styled-components'
 import h from 'react-hyperscript'
+
+import ConceptList from './ConceptList'
+import ConceptCreation from './ConceptCreation'
 import { TxList } from '../TxList.js'
 
 // styles
@@ -17,78 +19,12 @@ padding:1.1em 3.5em;
 background: white;
 `
 
-const HeaderTitle = styled('div')`
-font-size:1.5em;
-`
-
-const Step = styled('span')`
-font-size:0.7em;
-border-bottom:${props => props.underlined? '2px solid #C4C4C4':null };
-margin-right:1em;
-`
-
-const ConceptCreationCardFrame = styled('div')`
-background: #FFFFFF;
-box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
-border-radius: 2px;
-margin: 2.2em auto; 
-width:13em;
-text-align:left;
-`
-
-const ConceptTitleBox = styled('div')`
-background: #C4C4C4;  
-padding:1.9em;
-`
-const TitleCaption = styled('div')`
-color: #444444;
-font-size:0.6em;
-`
-const ConceptTitle = styled('div')`
-color: #444444;
-font-size:1.1em;
-`
-const BottomPart = styled('div')`
-padding: 0.5em 0.25em;
-`
-const Question1 = styled('div')`
-margin-bottom:1.1em;
-color:#666666;
-font-size:0.7em;
-`
-const ButtonGroup = styled('div')`
-border-radius: 2px
-border: 1px solid #C4C4C4;
-width:12em;
-`
-const GetAssessedButton = styled('div')`
-display:inline-block;
-padding: 0.5em 0.25em;
-width:6em;
-text-align:center;
-`
-const LearnButton = styled('div')`
-display:inline-block;
-background: #C4C4C4;
-padding: 0.5em 0.25em;
-width:5em;
-text-align:center;
-`
-
-
 export class ConceptBoard extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      selectedConceptAddress: "0",
-      step:1
+      selectedConceptAddress: "0"
     }
-  }
-
-  createAssessment (e) {
-    this.props.loadConceptContractAndCreateAssessment(
-      Object.keys(this.props.concepts)[this.state.selectedConceptKey]
-    )
   }
 
   selectConceptAddress(e){
@@ -102,30 +38,7 @@ export class ConceptBoard extends Component {
 
       let ConceptHeader=h('div',"Choose a Concept")
       if (this.state.selectedConceptAddress!=="0"){
-        ConceptHeader=h('div',{style:{textAlign:"center"}},[
-          h(HeaderTitle,"LET'S CREATE YOUR ASSESSMENT"),
-          h('div',[
-            h(Step,{underlined:this.state.step===1},"STEP 1"),
-            h(Step,{underlined:this.state.step===2},"STEP 2"),
-            h(Step,{underlined:this.state.step===3},"STEP 3")
-          ]),
-          h(ConceptCreationCardFrame, [
-            h(ConceptTitleBox,[
-              h(TitleCaption,"CONCEPT"),
-              h(ConceptTitle,this.props.concepts[this.state.selectedConceptAddress])
-            ]),
-            h(BottomPart,[
-              h(Question1,"How much do you wish to pay each assessor?"),
-              h(ButtonGroup,[
-                h(GetAssessedButton,{
-                  //onClick:this.props.selectConcept.bind(this),
-                  //id:this.props.conceptAddress
-                },"Get Assessed"),
-                h(LearnButton,"Learn")
-              ])
-            ])
-          ])
-        ])
+        ConceptHeader=h(ConceptCreation,{conceptName:this.props.concepts[this.state.selectedConceptAddress]})
       }
 
       return h('div', [
---
+++ app/components/ConceptBoard/ConceptCreation.js
@@ -0,0 +1,152 @@
+import { Component } from 'react'
+import ConceptList from './ConceptList'
+import styled from 'styled-components'
+import h from 'react-hyperscript'
+import { TxList } from '../TxList.js'
+
+// styles
+
+const MainFrame = styled('div')`
+text-align:center;
+padding:4em 0;
+`
+
+const HeaderTitle = styled('div')`
+font-size:1.5em;
+`
+
+const StepBox = styled('div')`
+margin:2.2em 0 ;
+`
+
+const Step = styled('span')`
+font-size:0.7em;
+border-bottom:${props => props.underlined? '2px solid #C4C4C4':null };
+margin-right:1em;
+`
+
+
+const ConceptCreationCardFrame = styled('div')`
+background: #FFFFFF;
+box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.5);
+border-radius: 2px;
+margin: 0 auto; 
+width:13em;
+text-align:left;
+`
+
+const ConceptTitleBox = styled('div')`
+background: #C4C4C4;  
+padding:1.9em;
+`
+const TitleCaption = styled('div')`
+color: #444444;
+font-size:0.6em;
+`
+const ConceptTitle = styled('div')`
+color: #444444;
+font-size:1.1em;
+`
+const BottomPart = styled('div')`
+padding: 1.3em 1.1em;
+`
+const Question1 = styled('div')`
+margin-bottom:1.5em;
+color:#666666;
+font-size:0.7em;
+`
+const ButtonCaptionBox = styled('div')`
+font-size: 0.6em;
+width:100%;
+`
+const RightCaption = styled('span')`
+float: right;
+`
+
+const ButtonGroup = styled('div')`
+border-radius: 2px
+border: 1px solid #C4C4C4;
+width:100%;
+padding:0;
+margin-bottom:6.5em;
+`
+const AmountPerAssessor = styled('input')`
+display:inline-block;
+padding: 0.5em 0.5em;
+width:2.5em;
+text-align:left;
+border:none;
+text-decoration:none;
+font-size:1em;
+`
+const AHAUnit =styled('div')`
+padding: 0.5em 0.5em 0.5em 0;
+display:inline-block;
+`
+
+const TotalAmount = styled('div')`
+display:inline-block;
+background: #F2F2F2;
+padding: 0.5em 0.25em;
+width:4em;
+text-align:center;
+`
+
+
+export class ConceptCreation extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      step:1,
+      amountPerAssessor:5
+    }
+  }
+
+  createAssessment (e) {
+    this.props.loadConceptContractAndCreateAssessment(
+      Object.keys(this.props.concepts)[this.state.selectedConceptKey]
+    )
+  }
+
+  setAmountPerAssessor(e){
+    this.setState({amountPerAssessor:e.target.value})
+  }
+
+  render () {
+    return h(MainFrame,[
+          h(HeaderTitle,"LET'S CREATE YOUR ASSESSMENT"),
+          h(StepBox,[
+            h(Step,{underlined:this.state.step===1},"STEP 1"),
+            h(Step,{underlined:this.state.step===2},"STEP 2"),
+            h(Step,{underlined:this.state.step===3},"STEP 3")
+          ]),
+          h(ConceptCreationCardFrame, [
+            h(ConceptTitleBox,[
+              h(TitleCaption,"CONCEPT"),
+              h(ConceptTitle,this.props.conceptName)
+            ]),
+            h(BottomPart,[
+              h(Question1,"How much do you wish to pay each assessor?"),
+              h(ButtonCaptionBox,[
+                h('span','PAY'),
+                h(RightCaption,'TOTAL COST')
+              ]),
+              h(ButtonGroup,[
+                h('span',[
+                  h(AmountPerAssessor,{
+                    onChange:this.setAmountPerAssessor.bind(this),
+                    value:this.state.amountPerAssessor,
+                    type:"number",
+                    step:1
+                  }),
+                  h(AHAUnit,"AHA"),
+                ]),
+                h(TotalAmount,this.state.amountPerAssessor*5+" AHA")
+              ])
+            ])
+          ])
+        ])
+  }
+}
+
+export default ConceptCreation
