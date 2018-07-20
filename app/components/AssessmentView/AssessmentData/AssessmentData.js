3150e16505a5d9d00a6682c8701af250fa667384
Alex Singh <alex@alexsingh.com>
2018-07-20 15:37:02 -0300

    WIP: Refactored assessmentView styles

--- app/components/AssessmentView/AssessmentData/AssessmentData.js
+++ app/components/AssessmentView/AssessmentData/AssessmentData.js
@@ -4,7 +4,8 @@ import AssessorList from '../AssessorList'
 import ProgressButtonBar from '../ProgressButtonBar'
 import { StageDisplayNames } from '../../../constants.js'
 import { convertDate } from '../../../utils.js'
-import { SuperFrame, Header, Role, ConceptName, SubHeader, StatusIndicator, StatusKey, StatusValue, DataBox, InfoField, InfoKey, AssessorBox, InfoBox, InfoValue, AssessorsDone, MeetingPointButton } from './style.js'
+import styled from 'styled-components'
+import { Header, Role, ConceptName, SubHeader, StatusIndicator, StatusKey, StatusValue, DataBox, InfoField, InfoKey, AssessorBox, InfoBox, InfoValue, AssessorsDone, MeetingPointButton } from './style.js'
 
 var h = require('react-hyperscript')
 
@@ -21,44 +22,46 @@ export class AssessmentData extends Component {
       return (
         h(SuperFrame, [
           // holds role and concept title
-          h(Header, [
-            h(Role, assessment.assessee !== this.props.userAddress ? 'Assessing' : 'Getting assessed in'),
-            h(ConceptName, assessment.conceptData)
+          h(assessmentHeader, [
+            h(assessmentLabelRole, assessment.assessee !== this.props.userAddress ? 'Assessing' : 'Getting assessed in'),
+            h(assessmentTextTitle, assessment.conceptData)
           ]),
           // indicates status of assesssment
-          h(SubHeader, [
-            h(StatusIndicator, [
-              h(StatusKey, 'STATUS'),
-              h(StatusValue, statusString)
+          h(assessmentRowStatusDate, [
+            h(assessmentContainerStatus, [
+              h(assessmentLabelBody, 'STATUS'),
+              h(assessmentTextBody, statusString)
             ]),
-            h(StatusIndicator, [
-              h(StatusKey, 'Due Date:'),
-              h(StatusValue, convertDate(assessment.endTime))
+            h(assessmentContainerDate, [
+              h(assessmentLabelBody, 'Due Date:'),
+              h(assessmentTextBody, convertDate(assessment.endTime))
             ])
           ]),
           // basic info
-          h(DataBox, [
-            h(InfoBox, [
-              h(InfoField, [
-                h(InfoKey, 'Assessee'),
-                h(InfoValue, assessment.assessee)
+          h(assessmentContainerBody, [
+            h(assessmentColumnLeft, [
+              h(assessmentObjectText, [
+                h(assessmentLabelBody, 'Assessee'),
+                h(assessmentTextBody, assessment.assessee)
               ]),
-              h(InfoField, [
-                h(InfoKey, 'Fee'),
-                h(InfoValue, assessment.cost + 'AHA')
+              h(assessmentObjectText, [
+                h(assessmentLabelBody, 'Fee'),
+                h(assessmentTextBody, assessment.cost + 'AHA')
               ]),
-              h(InfoField, [
-                h(InfoKey, 'Meeting Point'),
-                h(InfoValue, assessment.data || '< no meeting point set >'),
+              h(assessmentObjectText, [
+                h(assessmentLabelBody, 'Meeting Point'),
+                h(assessmentTextBody, assessment.data || 'You haven\'t set a meeting point'),
                 h(MeetingPointButton, {href: assessment.data, disabled: assessment.data === ''}, 'View'),
                 assessment.assessee === this.props.userAddress
                   ? h(MeetingPointEditButton, {className: MeetingPointButton, assessee: assessment.assessee}) // TODO make this appear right of GotoMeetingPointButton
                   : null
               ])
             ]),
-            h(AssessorBox, [
-              h(AssessorsDone, 'Assessors'),
-              h(AssessorList)
+            h(assessmentColumnRight, [
+              h(assessmentObjectText, [
+                h(assessmentLabelBody, 'Assessors'),
+                h(assessmentObjectText)
+              ])
             ])
           ]),
           // progress-buttons
@@ -70,5 +73,51 @@ export class AssessmentData extends Component {
     }
   }
 }
+
+const SuperFrame = styled('div').attrs({className: 'flex flex-column w-100 mw8 bg-white shadow-4'})`
+font-family:'system-ui',sans-serif;
+`
+
+const assessmentHeader = styled('div').attrs({className: 'flex flex-column w-100 pv4 ph3 bg-lightest-blue'})`
+`
+
+const assessmentLabelRole = styled('h6').attrs({className: 'f6 tl ttu uppercase dark-blue mv0 fw4'})`
+`
+
+const assessmentTextTitle = styled('h2').attrs({className: 'f2 tl ttu uppercase dark-blue mt2 mb0 fw4'})`
+`
+
+const assessmentRowStatusDate = styled('div').attrs({className: 'flex flex-row w-100 bt bb b--gray items-center'})`
+`
+
+const assessmentContainerStatus = styled('div').attrs({className: 'flex flex-row w-50 items-center justify-between pa3'})`
+`
+
+const assessmentLabelBody = styled('h6').attrs({className: 'f6 mid-gray mv0 fw4 ttu uppercase'})`
+`
+
+const assessmentTextBody = styled('h5').attrs({className: 'f5 gray mt2 mb0 fw4 gray'})`
+`
+
+const assessmentContainerDate = styled('div').attrs({className: 'flex flex-row w-50 items-center justify-between pa3 bl b--gray'})`
+`
+
+const assessmentContainerBody = styled('div').attrs({className: 'flex flex-row w-100 items-center justify-between'})`
+`
+
+const assessmentColumnLeft = styled('div').attrs({className: 'flex flex-column w-50 items-around justify-around pa3'})`
+`
+
+const assessmentObjectText = styled('div').attrs({className: 'flex flex-column w-100  items-start justify-center self-start mv3'})`
+`
+
+const assessmentListAssessors = styled('div').attrs({className: 'flex flex-column w-100 h-100 self-start  items-start justify-center self-start mv3'})`
+`
+
+const assessmentColumnRight = styled('div').attrs({className: 'flex flex-column w-50 h-100 self-start items-start justify-around pa3'})`
+`
+
+const assessmentFooter = styled('div').attrs({className: 'flex flex-row w-100'})`
+`
 
 export default AssessmentData
--- app/components/AssessmentView/AssessmentData/style.js
+++ app/components/AssessmentView/AssessmentData/style.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components'
 
-export const SuperFrame = styled('div')`
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
-`
-
 export const Header = styled('div')`
   padding: 0.25em 1em;
   border: 2px solid palevioletred;
