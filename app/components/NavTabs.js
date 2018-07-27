793e780a24a0cca736b7f763a89e739d239830d3
Alex Singh <alex@alexsingh.com>
2018-07-27 10:11:58 -0300

    Updated navTab styling

--- app/components/NavTabs.js
+++ app/components/NavTabs.js
@@ -1,6 +1,6 @@
 import h from 'react-hyperscript'
 import { NavLink } from 'react-router-dom'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 export const NavTabs = () => {
   let activeStyle = {
@@ -8,10 +8,24 @@ export const NavTabs = () => {
     color: 'red'
   }
 
-  return h('div', [
-    h(NavLink, { to: '/concepts/', activeStyle: activeStyle}, 'Concepts '),
-    h(NavLink, { to: '/', activeStyle: activeStyle, exact: true }, 'Assessments '),
-    h(NavLink, { to: '/certificates/', activeStyle: activeStyle }, 'Certificates ')
+  const tabContainer = styled('div').attrs({className: 'flex flex-row self-center ba br1 blue mv2'})``
+
+  const tabObjectLeft = styled('div').attrs({className: 'flex items-center pv2 ph3 blue br'})``
+
+  const tabObjectMiddle = styled('div').attrs({className: 'flex items-center pv2 ph3 blue'})``
+
+  const tabObjectRight = styled('div').attrs({className: 'flex items-center pv2 ph3 blue bl'})``
+
+  return h(tabContainer, [
+    h(tabObjectLeft, [
+      h(NavLink, { to: '/concepts/', activeStyle: activeStyle}, 'Concepts ')
+    ]),
+    h(tabObjectMiddle, [
+      h(NavLink, { to: '/', activeStyle: activeStyle, exact: true }, 'Assessments ')
+    ]),
+    h(tabObjectRight, [
+      h(NavLink, { to: '/certificates/', activeStyle: activeStyle }, 'Certificates ')
+    ])
   ])
 }
 
