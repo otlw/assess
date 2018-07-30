0594f506dcf4111a2cf883802dc6fa6f2d9bb3c8
“joelamouche” <estienne.antoine@gmail.com>
2018-07-30 14:34:55 -0400

    added concept data info in reducer

--- app/reducers/conceptReducer.js
+++ app/reducers/conceptReducer.js
@@ -4,6 +4,28 @@ import {
 
 let initialState = {}
 
+/*
+  further concepts are stored like this:
+
+[
+	conceptAddress : {
+		description:string,
+		learnMore:string,
+		name:string
+	},
+	...
+]
+
+if the data is correctly stored on ipfs and
+
+[
+	conceptAddress:string,
+	...
+]
+
+
+ */
+
 function concepts (state = initialState, action) {
   switch (action.type) {
     case RECEIVE_CONCEPTS:
