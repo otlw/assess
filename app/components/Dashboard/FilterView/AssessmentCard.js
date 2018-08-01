059ffced43d7bf0ddd420d8605a5664e7c558063
“joelamouche” <estienne.antoine@gmail.com>
2018-08-01 12:31:47 -0400

    lint

--- app/components/Dashboard/FilterView/AssessmentCard.js
+++ app/components/Dashboard/FilterView/AssessmentCard.js
@@ -77,7 +77,6 @@ export class AssessmentCard extends Component {
 
 export default AssessmentCard
 
-
 const cardContainer = styled('div').attrs({
   className: 'flex flex-column ma3 br2 shadow-4'
 })`height: 420px; width: 300px;background: linear-gradient(180.1deg, #FFFFFF 0.05%, #E9F7FD 52.48%, #CFF9EF 85.98%);
