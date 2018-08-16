54fd4d723587bf7499b00160c1f1a4780f5f35a4
Alex Singh <alex@alexsingh.com>
2018-08-16 16:38:18 -0300

    updated Transaction styling

--- app/components/TxList.js
+++ app/components/TxList.js
@@ -1,13 +1,18 @@
 import { Component } from 'react'
+import styled from 'styled-components'
 import TxStatus from './TxStatus'
 var h = require('react-hyperscript')
 
 export class TxList extends Component {
   render () {
-    return h('div', this.props.transactions.map((tx) => {
+    return h(containerTransaction, this.props.transactions.map((tx) => {
       return h(TxStatus, {transaction: tx})
     }))
   }
 }
+
+const containerTransaction = styled('div').attrs({className: 'absolute dn w-100 h-100'})`
+background-color: #8CDCC9;
+`
 
 export default TxList
