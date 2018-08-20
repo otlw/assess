57eb72b348e0da987ffdd8410e1ee243ea434e17
Alex Singh <alex@alexsingh.com>
2018-08-20 12:31:24 -0700

    Finished styling of final score bar

--- app/components/AssessmentView/FinalResultBar.js
+++ app/components/AssessmentView/FinalResultBar.js
@@ -16,39 +16,42 @@ class FinalResultBar extends Component {
     let scoreString = this.props.finalScore + ' out of 100 ' + (this.props.finalScore > 50 ? 'and passed! Congratulations' : 'and failed. We can help you pass next time.')
     if (this.props.userAddress === this.props.assessee) {
       console.log()
-      return h(containerFinalScore, 'You scored ' + scoreString)
+      return h(scoreTextResult, 'You scored ' + scoreString)
     } else if (this.props.userStage === Stage.Done) {
       // user is assessor
       let gain = Math.round(this.props.payout - this.props.cost)
       return (
-        h(FinalResultBox, [
+        h(containerScore, [
           (this.cacheCommitData
-            ? h(AssessorScore, 'You scred: ' + Number(this.cacheCommitData))
+            ? h(scoreTextAssessor, 'You scored: ' + Number(this.cacheCommitData))
             : null), // user used a different way or machine to enter the score than our app
-          h(containerFinalScore, 'Final score is:' + scoreString),
-          h(EarnedReward, 'You earned ' + (gain >= 0 ? '+' : '-') + gain.toString() + ' AHA')
+          h(objectScoreText, [
+            h(scoreTextResult, 'The final score is ' + this.props.finalScore)
+          ]),
+          h(objectScoreText, [
+            h(scoreTextReward, 'You earned ' + (gain >= 0 ? '+' : '-') + gain.toString() + ' AHA')
+          ])
         ])
       )
     } else {
       // user is visitor
-      return h(containerFinalScore, 'The final score is ' + scoreString)
+      return h(scoreTextResult, 'The final score is ' + scoreString)
     }
   }
 }
 
 export default FinalResultBar
 
-export const containerFinalScore = styled('div').attrs({className: 'flex flex-row w-100 h3 items-center justify-center bt b--light-gray'})`
+export const containerScore = styled('div').attrs({className: 'flex flex-row w-100 h3 items-center justify-center bt b--light-gray'})`
 `
-export const FinalResultBox = styled('div')`
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+
+export const objectScoreText = styled('div').attrs({className: 'flex flex-row w-100 items-center justify-center bl b--light-gray'})`
 `
-export const AssessorScore = styled('span')`
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+
+export const scoreTextResult = styled('h5').attrs({className: 'f5 fw4 dark-gray'})`
 `
-export const EarnedReward = styled('span')`
-  padding: 0.25em 1em;
-  border: 2px solid palevioletred;
+
+export const scoreTextAssessor = styled('div').attrs({className: 'flex f-100'})`
+`
+export const scoreTextReward = styled('h5').attrs({className: 'f5 fw4 dark-gray'})`
 `
