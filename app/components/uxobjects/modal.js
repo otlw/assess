faefbdc10fd5093e0d08c278c9c6682c3c32d730
Alex Singh <alex@alexsingh.com>
2018-08-21 11:56:52 -0700

    Set export flags to component & constants

--- app/components/uxobjects/modal.js
+++ app/components/uxobjects/modal.js
@@ -2,8 +2,7 @@ import { Component } from 'react'
 import styled from 'styled-components'
 var h = require('react-hyperscript')
 
-// component to display all assessors
-class modal extends Component {
+export class modal extends Component {
   render () {
     return (
       h(appContainerObscurer, [
@@ -23,24 +22,24 @@ class modal extends Component {
   }
 }
 
-const appContainerObscurer = styled('div').attrs({className: 'absolute flex items-center justify-center pa4'})`
+export const appContainerObscurer = styled('div').attrs({className: 'absolute flex items-center justify-center pa4'})`
 background-color: hsla(0%, 0%, 0%, 0.9);`
 
-const modalContainer = styled('div').attrs({className: 'relative flex flex-column w-100 mw6 pa  br1 shadow-4 bg-near-white'})`
+export const modalContainer = styled('div').attrs({className: 'relative flex flex-column w-100 mw6 pa  br1 shadow-4 bg-near-white'})`
 `
 
-const modalHeader = styled('div').attrs({className: 'flex flex-column items-center justify-center pa3 bb b--light-gray bg-light-yellow'})`
+export const modalHeader = styled('div').attrs({className: 'flex flex-column items-center justify-center pa3 bb b--light-gray bg-light-yellow'})`
 `
 
-const modalTextTitle = styled('h4').attrs({className: 'f4 dark-gray tc'})``
+export const modalTextTitle = styled('h4').attrs({className: 'f4 dark-gray tc'})``
 
-const modalBody = styled('div').attrs({className: 'flex flex-column items-center justify-center pa3 b--gray ph5'})
+export const modalBody = styled('div').attrs({className: 'flex flex-column items-center justify-center pa3 b--gray ph5'})
 
-const modalTextBody = styled('p').attrs({className: 'f5 gray tl lh-copy'})``
+export const modalTextBody = styled('p').attrs({className: 'f5 gray tl lh-copy'})``
 
-const modalFooter = styled('div').attrs({className: 'flex flex-column items-center justify-center pa4-ns pa2'})``
+export const modalFooter = styled('div').attrs({className: 'flex flex-column items-center justify-center pa4-ns pa2'})``
 
-const modalButtonPrimary = styled('button').attrs({className: 'flex ph4 pv2 fw4 f5 shadow-4 items-center align-center br-pill bg-dark-blue near-white ttu uppercase'})`
+export const modalButtonPrimary = styled('button').attrs({className: 'flex ph4 pv2 fw4 f5 shadow-4 items-center align-center br-pill bg-dark-blue near-white ttu uppercase'})`
 background-color: #116187;`
 
 export default modal
