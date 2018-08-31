95bd410bbf145a48a2b2fc5d55b95fc064f1e782
Michiya Hibino <mphibino@gmail.com>
2018-08-31 16:12:09 -0400

    add first test for assessmentActions

---
+++ app/actions/assessmentActions.spec.js
@@ -0,0 +1,16 @@
+import * as actions from './assessmentActions'
+
+describe('assessmentActions', () => {
+
+	test('should create an action to receive an assessor', () => {
+		const address = '0x9f0D'
+		const assessor = '0xf2a'
+		const expectedAction = actions.receiveAssessor(address, assessor)
+		expect(expectedAction).toEqual({
+			type: actions.RECEIVE_ASSESSOR,
+			address,
+			assessor
+		})
+	})
+
+})
