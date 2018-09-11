5fd341d2d5850f20a08edc2ae6f1792f20b695b2
Alex Singh <alex@alexsingh.com>
2018-09-11 11:39:47 -0300

    Initial styling of helperBar

--- app/components/Helpers/HelperBar/HelperBar.js
+++ app/components/Helpers/HelperBar/HelperBar.js
@@ -1,5 +1,7 @@
 import { Component } from 'react'
 import styled from 'styled-components'
+import icoClose from '../../../assets/ico-close.svg'
+
 var h = require('react-hyperscript')
 
 export class HelperBar extends Component {
@@ -28,20 +30,18 @@ export class HelperBar extends Component {
       if (this.state.showHelper) {
         return (
           h(barContainer, [
-            h(barObject, [
+            h(barContainerTitle, [
+              h(icoQuestion, '?'),
               h(barTextTitle, topic.title)
             ]),
             h(barObject, [
-              h(barTextDescription, topic.text)
+              h(barTextDescription, topic.text),
+              (topic.followUp
+                ? h(barButtonPrimary, {onClick: this.nextScreen.bind(this)}, topic.followUp.linkText)
+                : null)
             ]),
-            (topic.followUp
-              ? h(barObject, [
-                h(barButtonPrimary, {onClick: this.nextScreen.bind(this)}, topic.followUp.linkText)
-              ])
-              : null),
-            h(barObject, [
-              h(barButtonClose, {onClick: this.gotIt.bind(this)}, 'Got it!') // TODO this could set a mark in state that the info has been seen already
-            ]) //,
+            h(barButtonClose, {onClick: this.gotIt.bind(this)}, h('img', {alt: 'icoClose', src: icoClose, className: 'h1 ma1'})) // TODO this could set a mark in state that the info has been seen already
+            //
             // h('button', {onClick: this.resetVisits.bind(this)}, 'resetToNoob!') // this is for us, so we can pretend being a first timer
           ])
         )
@@ -59,19 +59,33 @@ export default HelperBar
 
 // style
 
-export const barContainer = styled('div').attrs({className: 'absolute flex flex-row items-center justify-around w-100 pv3 bg-light-green'})`
+export const barContainer = styled('div').attrs({className: 'absolute flex flex-row items-center justify-between w-100 pv3 bg-light-green shadow-3'})`
 `
 
 export const barObject = styled('div').attrs({className: 'flex items-center justify-center mh2'})`
 `
 
-export const barTextDescription = styled('h5').attrs({className: 'f5 dark-green'})`
+export const barContainerTitle = styled('div').attrs({className: 'flex items-center justify-center mh2 ba br1 b--green'})`
+flex-shrink: 0;
 `
 
-export const barTextTitle = styled('h4').attrs({className: 'f5 dark-green'})`
-`
-export const barButtonClose = styled('button').attrs({className: 'flex items-center justify-center pa2 ba br-100 b--dark-green bg-none'})`
+export const icoQuestion = styled('div').attrs({className: 'flex items-center justify-center w2 h2 mr2 bg-green light-green br'})`
 `
 
-export const barButtonPrimary = styled('button').attrs({className: 'flex ph4 pv2 fw4 f5 shadow-4 items-center align-center br-pill bg-green near-white ttu uppercase'})`
+export const barTextTitle = styled('h4').attrs({className: 'f5 fw4 dark-green mv0 ph2'})`
+`
+
+export const barTextDescription = styled('h5').attrs({className: 'f5 fw4 dark-green mv0'})`
+flex-shrink: 2;
+`
+
+export const barButtonClose = styled('button').attrs({className: 'flex items-center justify-center w2 h2 mh2 ba br-100 b--near-black bg-transparent'})`
+outline: 0px;
+flex-shrink: 0;
+transition: 0.2s ease-in-out;
+:hover {background-color:hsla(158, 70%, 65%, 1); cursor:pointer;}
+`
+
+export const barButtonPrimary = styled('button').attrs({className: 'flex f5 bg-transparent bn dark-green ttu underline uppercase bw1 b--dark-green cursor'})`
+outline: 0px;
 `
