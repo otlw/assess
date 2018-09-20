0d3e429e05ea815dd0e92c0d299c24fb1b4aa371
Alex Singh <alex@alexsingh.com>
2018-09-20 13:49:38 -0300

    Finished refactoring assessmentView core

--- app/components/AssessmentView/AssessmentView.js
+++ app/components/AssessmentView/AssessmentView.js
@@ -34,59 +34,59 @@ export class AssessmentData extends Component {
       h(SuperFrame, [
         // holds role and concept title
         h(assessmentHeader, [
-          h(assessmentLabelRole, isAssessee ? 'Getting assessed in' : 'Assessing'),
-          h(assessmentTextTitle, assessment.conceptData.name)
-        ]),
-        // indicates status of assesssment
-        h(assessmentRowSubHeader, [
-          h(assessmentContainerStatus, [
-            h(assessmentLabelBody, 'STATUS'),
-            h(assessmentTextBody, statusString)
-          ]),
-          h(assessmentContainerDate, [
-            h(assessmentLabelBody, assessment.stage === Stage.Done ? 'Completed on: ' : 'Due Date:'),
-            h(assessmentTextBody, convertDate(assessment.checkpoint))
+          h(assessmentLabelActivity, isAssessee ? 'Getting assessed in' : 'Assessing'),
+          h(assessmentTextTitle, assessment.conceptData.name),
+          // indicates status of assesssment
+          h(assessmentRowSubHeader, [
+            h(assessmentContainerStatus, [
+              h(assessmentContainerStatusIndicator, [
+                h(assessmentLabelStatus, 'STATUS'),
+                h(assessmentIndicatorActive),
+                h(assessmentIndicatorInactive),
+                h(assessmentIndicatorInactive),
+                h(assessmentIndicatorInactive)
+              ]),
+              h(assessmentTextBody, statusString)
+            ]),
+            h(assessmentContainerDate, [
+              h(assessmentLabelBody, assessment.stage === Stage.Done ? 'Completed on: ' : 'DEADLINE'),
+              h(assessmentTextBody, convertDate(assessment.checkpoint))
+            ])
           ])
         ]),
         // basic info
         h(assessmentContainerBody, [
-          h(assessmentColumnLeft, [
-            h(assessmentObjectText, [
-              h(assessmentLabelBody, 'Assessee'),
-              h(assessmentTextBody, isAssessee ? 'You' : assessment.assessee)
-            ]),
-            h(assessmentObjectText, [
-              h(assessmentLabelBody, 'Fee'),
-              h(assessmentTextBody, assessment.cost + ' AHA')
-            ]),
-            h(assessmentObjectText, [
-              h(assessmentLabelBody, 'Meeting Point'),
-              h(assessmentTextBody, [
-                assessment.data
+          h(assessmentObjectText, [
+            h(assessmentLabelBody, 'Assessee'),
+            h(assessmentTextAddress, isAssessee ? 'You' : assessment.assessee)
+          ]),
+          h(assessmentObjectTextRight, [
+            h(assessmentLabelBody, 'Assessors'),
+            h(AssessorList, {
+              assessors: assessment.assessors,
+              userAddress: this.props.userAddress
+            })
+          ]),
+          h(assessmentObjectText, [
+            h(assessmentLabelBody, 'Meeting Point'),
+            h(assessmentTextBody, [
+              assessment.data
                   ? h('a', {href: assessment.data}, assessment.data)
                   : isAssessee
                     ? 'You haven\'t set a meeting point'
                     : 'No meeting point has been set yet']),
-              h(assessmentRow, [
-                assessment.assessee === this.props.userAddress
+            h(assessmentRow, [
+              assessment.assessee === this.props.userAddress
                   ? h(MeetingPointEditBox, {
                     assessee: assessment.assessee,
                     assessmentAddress: assessment.address
                   })
                   : null
-              ])
             ])
           ]),
-          h(assessmentColumnRight, [
-            h(assessmentObjectTextRight, [
-              h(assessmentLabelBody, 'Assessors'),
-              h(assessmentObjectText, [
-                h(AssessorList, {
-                  assessors: assessment.assessors,
-                  userAddress: this.props.userAddress
-                })
-              ])
-            ])
+          h(assessmentObjectTextRight, [
+            h(assessmentLabelBody, 'Fee'),
+            h(assessmentTextBody, assessment.cost + ' AHA')
           ])
         ]),
         // progress-button, FinalResultBor or FailureIndicator
@@ -118,55 +118,80 @@ export class AssessmentData extends Component {
 
 export default AssessmentData
 
-const SuperFrame = styled('div').attrs({className: 'flex flex-column w-100 mw8 self-center mt3 bg-white shadow-4'})`
+const SuperFrame = styled('div').attrs({className: 'flex flex-column w-100 mw8 self-center mt3 br2 shadow-4'})`
 font-family:'system-ui',sans-serif;
+max-width: 800px;
 `
 
-const assessmentHeader = styled('div').attrs({className: 'flex flex-column w-100 pv4 ph3 bg-lightest-blue'})`
+// assessmentView Header
+
+const assessmentHeader = styled('div').attrs({className: 'flex flex-column w-100 pv3 ph3 bg-lightest-blue br2 br--top'})`
 `
 
-const assessmentLabelRole = styled('h6').attrs({className: 'f6 tl ttu uppercase dark-blue mv0 fw4'})`
+const assessmentLabelActivity = styled('h6').attrs({className: 'f5 tl ttu uppercase dark-blue mv0 fw4'})`
 `
 
-const assessmentTextTitle = styled('h2').attrs({className: 'f2 tl ttu uppercase dark-blue mt2 mb0 fw4'})`
+const assessmentTextTitle = styled('h2').attrs({className: 'f2 tl dark-blue mt2 mb0 fw4'})`
 `
 
-const assessmentRowSubHeader = styled('div').attrs({className: 'flex flex-row w-100 items-center bt bb b--gray'})`
+const assessmentRowSubHeader = styled('div').attrs({className: 'flex flex-row w-100 items-center mt5'})`
 `
 
-const assessmentContainerStatus = styled('div').attrs({className: 'flex flex-row w-50 items-center justify-between pa3'})`
+const assessmentContainerStatus = styled('div').attrs({className: 'flex flex-row flex-wrap w-50 items-start justify-between '})`
 `
 
-const assessmentLabelBody = styled('h6').attrs({className: 'f6 mid-gray mv0 fw4 ttu uppercase'})`
+// assessmentView Header
+//  ->  Status Indicator
+
+const assessmentContainerStatusIndicator = styled('div').attrs({className: 'flex flex-row w-100 items-center'})`
+`
+
+const assessmentIndicatorInactive = styled('div').attrs({className: 'flex ba br-100 mh2'})`
+height: 10px;
+width: 10px;
+color: #322EE5;`
+
+const assessmentIndicatorActive = styled('div').attrs({className: 'flex ba br-100 mh2'})`
+height: 10px;
+width: 10px;
+color: #322EE5;
+background: #322EE5; 
+`
+
+const assessmentLabelStatus = styled('h6').attrs({className: 'f5 w-auto mid-gray mr2 mv0 fw4 tl ttu uppercase'})`
+`
+
+// End Status Indicator
+// End assessmentView Header
+
+const assessmentLabelBody = styled('h6').attrs({className: 'f5 w-100 mid-gray mv0 fw4 tl ttu uppercase'})`
 `
 
 const assessmentTextBody = styled('h5').attrs({className: 'f5 gray mv2 mb0 fw4 gray'})`
 `
 
-const assessmentContainerDate = styled('div').attrs({className: 'flex flex-row w-50 items-center justify-between pa3 bl b--gray'})`
+const assessmentTextAddress = styled('h5').attrs({className: 'f5 gray mv2 mb0 fw4 gray w-50 truncate ellipsis'})`
 `
 
-const assessmentContainerBody = styled('div').attrs({className: 'flex flex-row w-100 items-center justify-between'})`
+const assessmentContainerDate = styled('div').attrs({className: 'flex flex-column w-50 items-start justify-between '})`
 `
 
-const assessmentColumnLeft = styled('div').attrs({className: 'flex flex-column w-50 items-around justify-around pa3'})`
+const assessmentContainerBody = styled('div').attrs({className: 'flex flex-row flex-wrap w-100 items-center justify-between ph3 bg-white'})`
 `
 
-const assessmentObjectText = styled('div').attrs({className: 'flex flex-column w-100  items-start justify-center self-start mv2'})`
+const assessmentObjectText = styled('div').attrs({className: 'flex flex-column w-50  items-start justify-center self-start mv4'})`
 `
 
-const assessmentObjectTextRight = styled('div').attrs({className: 'flex flex-column w-100  items-start justify-center self-start mt3 mb0'})`
+const assessmentObjectTextRight = styled('div').attrs({className: 'flex flex-column w-auto  items-end justify-center self-start mv4'})`
 `
 
 // Commented out as we may need to re-implement this very soon
 // const assessmentListAssessors = styled('div').attrs({className: 'flex flex-column w-100 h-100
 // self-start  items-start justify-center self-start mv3'})``
 
-const assessmentColumnRight = styled('div').attrs({className: 'flex flex-column w-50 h-100 self-start items-start justify-around pa3'})`
-`
-
 const assessmentRow = styled('div').attrs({className: 'flex flex-row w-100 mw5 justify-between mt3 '})`
 `
 
-const assessmentFooter = styled('div').attrs({className: 'relative flex flex-row w-100'})`
+const assessmentFooter = styled('div').attrs({className: 'relative flex flex-row w-100 bg-white'})`
+margin-top:1px;
 `
