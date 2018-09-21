ec64cbcf4b8857dbae75602d0869c9a1c33b44fd
Jared Pereira <jared@otlw.co>
2018-09-21 10:11:52 +0400

    add Ethereum state to root reducer

--- app/store/index.ts
+++ app/store/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as reduxFormReducer } from 'redux-form'
-import { EthereumReducer } from './web3/reducer'
+import { EthereumReducer, EthereumState } from './web3/reducer'
 import { AssessmentsReducer, AssessmentsState } from './assessment/reducer'
 import {ConceptsReducer, ConceptsState }from './concept/reducer'
 import { TransactionsReducer, TransactionsState } from './transaction/reducer'
@@ -9,7 +9,7 @@ import loading from './loading/reducer'
 
 export type State = {
   form: any,
-  ethereum: any,
+  ethereum: EthereumState,
   assessments: AssessmentsState,
   concepts: ConceptsState,
   transactions: TransactionsState,
