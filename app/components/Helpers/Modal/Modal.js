d82444495e9e3ecfdd237baa64d101cfee9d2652
djudjuu <julfaber@gmail.com>
2018-09-24 17:53:57 +0200

    helperContent

---
+++ app/components/Helpers/Modal/Modal.js
@@ -0,0 +1,141 @@
+import h from 'react-hyperscript'
+import styled from 'styled-components'
+import { Component } from 'react'
+// import {Link} from 'react-router-dom'
+
+// TODO HOW can we make this component dissappear? when the user clicks on somthing from the navbar?
+// idea: if this is styled to be transparent, the user see's that stuff changes and that he has to click to close it.
+export class Modal extends Component {
+  closeScreen () {
+    this.props.setModal('')
+  }
+
+  nextScreen () {
+    this.closeScreen()
+    if (this.props.topic.followUp) {
+      this.props.setModal(this.props.topic.followUp.target)
+    }
+  }
+
+  render () {
+    console.log('rendering', this.props)
+    let link = linkButton(this.props.topic, this.closeScreen.bind(this))
+    return (
+      h(appContainerObscurer, [
+        h(modalContainer, [
+          h(modalHeader, [
+            h(modalTextTitle, this.props.topic.title),
+            h(modalHeaderObjectCircle),
+            h(modalHeaderObjectTri),
+            h(modalHeaderObjectTriLarger),
+            h(modalHeaderObjectSquare)
+          ]),
+          h(modalBody, [
+            h(modalTextBody, this.props.topic.text),
+            link
+          ]),
+          h(modalFooter, [
+            h(modalButtonSecondary, {onClick: this.closeScreen.bind(this)}, 'Close'),
+            this.props.topic.followUp
+              ? h(modalButtonPrimary, {onClick: this.nextScreen.bind(this)}, 'Learn More')
+              : null
+          ])
+        ])
+      ])
+    )
+  }
+}
+
+function linkButton (topic, closeFunction) {
+  /*
+    This would be how we treat extra logic being passed along with the topic
+    example: the link to the created assessment
+    if (topic.title === 'AssessmentCreation') {
+    if (topic.params.success) {
+      return h(Link, {
+        to: '/assessment/' + topic.params.assessmentId,
+        onClick: closeFunction
+      }, 'Go')
+    } else {
+      console.log('creation failed due to ', topic.params.error)
+    }
+  }
+  */
+  return null
+}
+
+export default Modal
+
+export const appContainerObscurer = styled('div').attrs({className: 'absolute flex w-100 h-100 items-center justify-center pa4 z-999'})`
+top:0px;
+background-color: hsla(0, 0%, 10%, 0.8);
+`
+
+export const modalContainer = styled('div').attrs({className: 'relative flex flex-column w-100 mw6 pa br1 shadow-5 bg-near-white'})`
+`
+
+export const modalHeader = styled('div').attrs({className: 'relative flex flex-column items-center justify-center pa3 br1 bb b--light-gray'})`
+min-height: 240px;
+background: linear-gradient(144.78deg,hsla(246,58%,52%,0.7) 0%,hsla(246, 30%, 87%, 1) 100%);
+`
+
+export const modalHeaderObjectCircle = styled('div').attrs({className: 'absolute flex w1 h1 br-100 z-9'})`
+top: 24px;
+left: 20px;
+background: hsla(245, 61%, 57%,0.25);
+transform: skew(32deg, -12deg);
+`
+
+export const modalHeaderObjectSquare = styled('div').attrs({className: 'absolute flex w2 h2 z-9 o-30'})`
+top: 40px;
+right: 80px;
+background: hsla(245, 61%, 57%,0.25);
+transform: skew(59deg,-28deg);
+`
+
+export const modalHeaderObjectTri = styled('div').attrs({className: 'absolute o-50'})`
+width: 0;
+height: 0;
+border-left: 16px solid transparent;
+border-right: 16px solid transparent;
+border-bottom: 16px solid hsla(245, 61%, 57%,0.25);
+right: 96px;
+bottom:24px;
+transform:rotate(24deg);
+filter:blur(1px);
+`
+
+export const modalHeaderObjectTriLarger = styled('div').attrs({className: 'absolute o-50'})`
+width: 0;
+height: 0;
+border-left: 24px solid transparent;
+border-right: 24px solid transparent;
+border-bottom: 24px solid hsla(245, 61%, 57%,0.25);
+left: 80px;
+bottom:24px;
+transform:rotate(147deg);
+filter:blur(1px);
+`
+
+export const modalTextTitle = styled('h4').attrs({className: 'f3 dark-gray tc'})`
+`
+
+export const modalBody = styled('div').attrs({className: 'flex flex-column items-center justify-center pa3 b--gray ph5'})`
+min-height:120px;
+`
+
+export const modalTextBody = styled('p').attrs({className: 'f4 gray tl lh-copy'})``
+
+export const modalFooter = styled('div').attrs({className: 'flex flex-row items-center justify-around pa4-ns pa2'})``
+
+export const modalButtonPrimary = styled('button').attrs({className: 'flex ph4 pv2 fw4 f5 mh2 shadow-4 items-center align-center br-pill bg-green near-white ttu uppercase'})`
+background-color: #116187;
+:hover {cursor:pointer;}
+`
+
+const modalButtonSecondary = styled('div').attrs({
+  className: 'flex ph4 pv2 fw4 f5 mh2 items-center align-center br-pill dark-gray'
+})`box-shadow: 0px 0px 0px 1px hsla(214, 100%, 31%, 0.1);
+transition: 0.2s ease-in-out;
+:hover {cursor:pointer; background: #ccc; color: #444;}
+`
---
+++ app/components/Helpers/helperContent.js
@@ -0,0 +1,105 @@
+export const modalTopic = Object.freeze({
+  UnlockMetaMask: 'UnlockMetaMask',
+  AssessmentProcess: 'AssessmentProcess',
+  NoMetaMask: 'NoMetaMask',
+  EducateAboutMetaMask: 'EducateAboutMetaMask',
+  UndeployedNetwork: 'UndeployedNetwork',
+  AssessmentCreation: 'AssessmentCreation'
+})
+
+// this functions delivers simple one-liner explanations on given topics + a keyWord under which more can
+// be learned
+// @params: topic can be a string OR an object with keys: topic and params (e.g. when one needs to create an assessment)
+export function helperText (topic) {
+  // let topic = typeof _topic === 'object' ? _topic.topic : _topic
+  // allow topic to be an object that holds parameters (such as an address)
+  switch (topic) {
+    case modalTopic.UnlockMetaMask:
+      return {
+        title: 'MetaMask is locked',
+        text: 'Please unlock Metamask by entering your password.\n',
+        followUp: {
+          target: modalTopic.educateAboutMetaMask // again, this is just to test the link funcitonality
+        }
+      }
+    case modalTopic.NoMetaMask:
+      return {
+        title: topic,
+        text: "You don't have the MetaMask browser extension that allows to use this app.\n Please Download it to use the features of this interface"
+      }
+    case modalTopic.educateAboutMetaMask:
+      return {
+        title: topic,
+        text: 'There is this cool thing called Metamask, we need so you can be safe and foxxy.'
+      }
+    case modalTopic.UndeployedNetwork:
+      return {
+        title: topic,
+        text: "You're connected to a network on which you haven't deployed contracts. Please use an appropriate script."
+      }
+    case modalTopic.AssessmentProcess:
+      return {
+        title: topic,
+        text: 'Here are some cool graphs and texts that show you how an assessment works'
+      }
+    case modalTopic.AssessmentCreation:
+      return {
+        title: topic,
+        text: 'Success! Your assessment has been created. Click this link (TODO) to view the details.'
+      }
+    case modalTopic.AssessmentCreationFailed:
+      return {
+        title: topic,
+        text: 'Ooopsi'
+      }
+    default:
+      if (topic) console.log('no helperText defined for topic', topic)
+      return false
+  }
+}
+
+// export const barTopic = Object.freeze({
+//   Staking: 'Staking',
+//   Committing: 'Committing',
+//   Revealing: 'Revealing',
+//   ConfirmedStake: 'ConfirmedStake',
+//   ConfirmedCommit: 'ConfirmedCommit',
+//   ConfirmedReveal: 'ConfirmedReveal',
+//   FirstTimeMeetingPointSet: 'FirstTimeMeetingPointSet',
+//   MeetingPointChanged: 'MeetingPointChanged',
+//   ChallengePeriodActive: 'ChallengePeriodActive',
+//   Smues: 'Smues'
+// })
+
+// // topics for the helperBar
+//     case barTopic.Staking:
+//       return {
+//         title: 'What\'s staking?',
+//         text: 'Staking is where each Assessor needs to pay a small fee to assess you. It’s done so that they have “skin in the game” and are more likely to assess you fairly.',
+//         followUp: {
+//           target: modalTopic.AssessmentProcess
+//         }
+//       }
+//     case barTopic.ConfirmedStake:
+//       // not sure that this is what we want to do, just wanted to test the feedback on immediate action
+//       return {
+//         title: 'Awesome!',
+//         text: 'Now you just need to wait until there are enough other assessors and the assessment starts. If you\'re curious you can learn already learn what you need to do in the next stage: The commit-stage.',
+//         followUp: {
+//           target: barTopic.Committing
+//         }
+//       }
+//     case barTopic.Committing:
+//       return {
+//         title: 'What\'s committing?',
+//         text: 'This is were you rate the assessee. Give him 100 points if you think he is smüs.',
+//         followUp: {
+//           linkText: 'What the hell is "smüs"?',
+//           target: barTopic.Smues
+//         }
+//       }
+//     case barTopic.Smues:
+//       return {
+//         title: 'You\'re a curious person. Cool!',
+//         text: 'That\'s best explained by an example. Take this totally smüs sentence: "Is it smüs how saying sentences backwards creates backwards sentences saying how smüs it is? \n'
+//       }
