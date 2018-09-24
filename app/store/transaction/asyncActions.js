542fdef2d70b9c652472870252a042cee3ea4fbd
Michiya Hibino <mphibino@gmail.com>
2018-09-24 15:14:12 -0400

    add spaces for discussion comments

--- app/store/transaction/asyncActions.js
+++ app/store/transaction/asyncActions.js
@@ -8,27 +8,34 @@ import { saveTransaction, updateTransaction } from './actions'
    @confirmationCallback: a function to be called once the transaction has been confirmed
 */
 export function sendAndReactToTransaction (dispatch, act, saveData, userAddress, assessmentAddress, confirmationCallback) {
+
   // act.method(...act.args).send({from: userAddress, gas: gas || 320000})
   act()
     .on('transactionHash', (hash) => {
+
       // right after the transaction is published
       // confirmationCallback(false, hash)
       dispatch(saveTransaction(assessmentAddress, userAddress, saveData, hash))
     })
     .on('confirmation', (confirmationNumber, receipt) => {
+
       // TODO: choose a good confirmation number (kovan and rinkeby accept 2, but local textnet requires 8)
       // when the transaction is confirmed into a block
       if (confirmationNumber === 8) {
+
         dispatch(updateTransaction(
           receipt.transactionHash,
           receipt.status ? 'Tx confirmed' : 'Tx failed'
         ))
       }
       if (confirmationCallback && confirmationNumber === 9) {
+
         if (receipt.status) {
           confirmationCallback(false, receipt)
+
         } else {
           confirmationCallback(true, receipt)
+
         }
       }
     })
