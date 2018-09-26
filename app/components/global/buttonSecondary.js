f5cb4b0a2c9686a7c628964f4ca8679f49e3bbe9
Alex Singh <alex@alexsingh.com>
2018-09-26 17:26:12 -0300

    Completed 'Secondary Button'

---
+++ app/components/global/buttonSecondary.js
@@ -0,0 +1,19 @@
+import { Component } from 'react'
+import styled from 'styled-components'
+import h from 'react-hyperscript'
+
+export class compButtonSecondary extends Component {
+  render () {
+    return h(buttonSecondary, 'buttonText'
+    )
+  }
+}
+
+export default compButtonSecondary
+
+// styles
+
+const buttonPrimary = styled('button').attrs({className: 'flex pv2 ph4 items-center justify-center br-pill bn ttu uppercase pointer shadow-1'})`
+color: ${props => props.theme.primary};
+background-color: ${props => props.theme.secondary};
+`
