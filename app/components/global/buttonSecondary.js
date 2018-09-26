45258641ccb47864d4a97e0c40a91c9989a5087f
Alex Singh <alex@alexsingh.com>
2018-09-26 17:31:04 -0300

    buttonSecondary fix

--- app/components/global/buttonSecondary.js
+++ app/components/global/buttonSecondary.js
@@ -13,7 +13,7 @@ export default compButtonSecondary
 
 // styles
 
-const buttonPrimary = styled('button').attrs({className: 'flex pv2 ph4 items-center justify-center br-pill bn ttu uppercase pointer shadow-1'})`
+const buttonSecondary = styled('button').attrs({className: 'flex pv2 ph4 items-center justify-center br-pill bn ttu uppercase pointer shadow-1'})`
 color: ${props => props.theme.primary};
 background-color: ${props => props.theme.secondary};
 `
