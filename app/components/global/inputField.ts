33e2c34e0eaa43ab2df6f114ac5cb858151c2696
Alex Singh <alex@alexsingh.com>
2018-09-28 09:42:02 -0300

    Commented in to-do's

--- app/components/global/inputField.ts
+++ app/components/global/inputField.ts
@@ -12,6 +12,9 @@ export default inputField
 
 // styles
 
+// TODO: Low priority: being able to set custom widths as different input fields need to handle different length inputs // (this is a UX fundamental. Refer to 'Proper Field Size' here: 
+// https://uxplanet.org/designing-perfect-text-field-clarity-accessibility-and-user-effort-d03c1e26004b
+
 const styleInputField = styled('input').attrs({className: 'flex w4 bn br2 pa2 mr2'})`
 outline: none;
 color: ${props => props.theme.primary};
--- app/components/global/progressBar.ts
+++ app/components/global/progressBar.ts
@@ -15,6 +15,10 @@ export default progressBar
 const styleProgressBar = styled('div').attrs({className: 'flex flex-row w-auto items-center'})`
 `
 
+// TODO
+// 1. Need a dynamic way to set the # of progress dots for different flows
+// 2. What's the best implementation for setting active/inactive states? A single const for the 'dot' that has props passed to it to set it active/inactive? I'm not sure how to implement that.
+
 const assessmentIndicatorInactive = styled('div').attrs({className: 'flex ba br-100 mh2'})`
 height: 10px;
 width: 10px;
