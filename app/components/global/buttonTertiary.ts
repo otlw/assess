dd08a48e6a3533690a3eee13675ac0abf4faa637
Alex Singh <alex@alexsingh.com>
2018-10-04 10:50:20 -0300

    Updated buttonTertiary styling

--- app/components/global/buttonTertiary.ts
+++ app/components/global/buttonTertiary.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
 
-const ButtonTertiary = styled('button').attrs({className: 'flex pv2 ph3 items-center justify-center br4 ttu uppercase pointer'})`
+const ButtonTertiary = styled('button').attrs({className: 'flex pv2 ph3 items-center justify-center f6 br1 bg-transparent ttu uppercase pointer'})`
 color: ${props => props.theme.primary};
 border: 1px solid ${props => props.theme.primary};
+transition: 0.2s ease-in-out;
+:hover {
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.tertiary};
+  }
 `
 
 export default ButtonTertiary
