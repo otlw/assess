921127eac7bb39495770129c906be2097b701750
Alex Singh <alex@alexsingh.com>
2018-10-04 11:39:46 +0000

    Adding misc. global components

--- app/components/global/buttonHelp.ts
+++ app/components/global/buttonHelp.ts
@@ -1,20 +1,10 @@
 import styled from 'styled-components'
-import h from 'react-hyperscript'
 
-type Props = {
-  text: string
-}
-const buttonHelp = (props:Props) => {
-    return h(styleButtonHelp, props.text)
-}
-
-export default buttonHelp
-
-// styles
-
-const styleButtonHelp = styled('button').attrs({className: 'flex items-center justify-center content-center f6 fw1 br-100 bn ttu uppercase pointer'})`
+const ButtonHelp = styled('button').attrs({className: 'flex items-center justify-center content-center f6 fw1 br-100 bn ttu uppercase pointer'})`
 width: 24px;
 height: 24px;
 color: ${props => props.theme.secondary};
 background-color: ${props => props.theme.primary};
 `
+
+export default ButtonHelp
