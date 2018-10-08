c23ebc4a07866103f42b0d6102e36cd15ce4dfad
Alex Singh <alex@alexsingh.com>
2018-10-08 17:52:14 -0300

    Tiny styling fix

--- app/components/Global/Links.ts
+++ app/components/Global/Links.ts
@@ -8,7 +8,7 @@ color: ${props => props.theme.tertiary};
 background-color: ${props => props.theme.primary};
 `
 
-export const LinkSecondary = styled(Link).attrs({className: 'flex pv2 ph4 items-center justify-center br-pill bn ttu uppercase pointer shadow-1 link'})`
+export const LinkSecondary = styled(Link).attrs({className: 'flex pv2 ph4 items-center justify-center br-pill bn pointer shadow-1 link'})`
 color: ${props => props.theme.primary};
 background-color: ${props => props.theme.tertiary};
 `
