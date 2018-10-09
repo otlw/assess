de467f63b012a3bbba35579e3871e1b7550a3d57
Michiya Hibino <mphibino@gmail.com>
2018-10-09 16:40:13 -0400

    add global component

--- app/components/Helpers/HelperBar/HelperBar.ts
+++ app/components/Helpers/HelperBar/HelperBar.ts
@@ -2,8 +2,7 @@ import { Component } from 'react'
 import styled from 'styled-components'
 import {Label, Body} from '../../Global/Text'
 import {Props} from './index'
-let icoClose = require('../../../assets/ico-close.svg')
-
+import { ButtonClose } from '../../Global/Buttons'
 var h = require('react-hyperscript')
 
 export class HelperBar extends Component<Props> {
@@ -28,15 +27,15 @@ export class HelperBar extends Component<Props> {
           h(barContainer, [
             h(barContainerTitle, [
               h(icoQuestion, '?'),
-              h(Label, topic.title) // decrease size: 'h5' --> 'h4' ?
+              h(Label, topic.title) // decrease header size? (e.g., 'h5' --> 'h4')
             ]),
             h(barObject, [
-              h(Body, topic.text), // decrease font size: f6 --> f5 ?
+              h(Body, topic.text), // decrease font size? (e.g., f6 --> f5)
               (topic.learnMore
                ? h(barButtonPrimary, {onClick: this.learnMore.bind(this)}, 'Learn More')
                : null)
             ]),
-            h(barButtonClose, {onClick: this.closeBar.bind(this)}, h('img', {alt: 'icoClose', src: icoClose, className: 'h1 ma1'}))
+            h(ButtonClose, {onClick: this.closeBar.bind(this)})
             //
             // h('button', {onClick: this.resetVisits.bind(this)}, 'resetToNoob!') // this is for us, so we can pretend being a first timer
           ])
@@ -70,13 +69,6 @@ border-color: ${props => props.theme.primary};
 export const icoQuestion = styled('div').attrs({className: 'flex items-center justify-center w2 h2 mr2 bg-green light-green br'})`
 color: ${props => props.theme.tertiary};
 background-color: ${props => props.theme.primary};
-`
-
-export const barButtonClose = styled('button').attrs({className: 'flex items-center justify-center w2 h2 mh2 ba br-100 b--near-black bg-transparent'})`
-outline: 0px;
-flex-shrink: 0;
-transition: 0.2s ease-in-out;
-:hover {background-color:hsla(203, 100%, 96%, 1); cursor:pointer;}
 `
 
 export const barButtonPrimary = styled('button').attrs({className: 'flex f6 bg-transparent ph1 pv1 ml1 ttu uppercase ba br1 b--dark-green cursor'})`
