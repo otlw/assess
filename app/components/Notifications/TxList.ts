0e3cb37f6e74bd7d42425ecbd486e5a61c9b5420
Michiya Hibino <mphibino@gmail.com>
2018-10-09 16:41:16 -0400

    remove console log

--- app/components/Notifications/TxList.ts
+++ app/components/Notifications/TxList.ts
@@ -10,7 +10,6 @@ interface ITxListProps {
 
 export class TxList extends Component<ITxListProps> {
   render () {
-    console.log('TxList -- props: ', this.props) // DEBUG
     // this should not be necessary but somehow if this next line is not here
     // a big green empty field will be displayed
     if (this.props.transactions.length === 0) { return null }
