d156ba99e31d690d7c83bca73f95ce373c99c589
“joelamouche” <estienne.antoine@gmail.com>
2018-10-11 11:25:55 +0200

    fixed merge

--- app/components/Global/Text.ts
+++ app/components/Global/Text.ts
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 
-export const Body = styled('h5').attrs<{feedBack?:string}>({className: 'f5 fw4 mv0'})`
-color: ${props => props.feedBack ? (props.feedBack === 'success' ? props.theme.positiveGreenContrast : props.theme.negativeRedContrast) : props.theme.textBody};
+export const Headline = styled('h2').attrs({className: 'f2 fw4 mt3 mb0'})`
+color: ${props => props.theme.primary};
 `
 
 export const Subheadline = styled('h3').attrs({className: 'f3 fw4 mt3 mb0'})`
