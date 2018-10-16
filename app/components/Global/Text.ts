8460d03b9d82b134a358ff62e947c5556261c831
“joelamouche” <estienne.antoine@gmail.com>
2018-10-16 11:27:33 +0200

    ran the linter

--- app/components/Global/Text.ts
+++ app/components/Global/Text.ts
@@ -1,19 +1,19 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 
-export const Headline = styled('h2').attrs({className: 'f2 fw4 mt3 mb0'})`
+export const Headline = styled("h2").attrs({className: "f2 fw4 mt3 mb0"})`
 color: ${props => props.theme.primary};
-`
+`;
 
-export const Subheadline = styled('h3').attrs({className: 'f3 fw4 mt3 mb0'})`
+export const Subheadline = styled("h3").attrs({className: "f3 fw4 mt3 mb0"})`
 color: ${props => props.theme.secondary};
-`
+`;
 
-export const Body = styled('h5').attrs({className: 'f5 fw4 mv2'})`
+export const Body = styled("h5").attrs({className: "f5 fw4 mv2"})`
 color: ${props => props.theme.textBody};
-`
+`;
 
 
-export const Label = styled('h5').attrs({className: 'f5 fw4 mv0 ttu uppercase'})`
+export const Label = styled("h5").attrs({className: "f5 fw4 mv0 ttu uppercase"})`
 color: ${props => props.theme.primary};
-`
+`;
