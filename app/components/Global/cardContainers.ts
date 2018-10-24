11fd9e5e62f697e9e2fc9933b49b744611815759
“joelamouche” <estienne.antoine@gmail.com>
2018-10-24 12:27:52 +0200

    put structure inside explanationCard component

--- app/components/Global/cardContainers.ts
+++ app/components/Global/cardContainers.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { Component } from 'react'
 import h from 'react-hyperscript'
+import {ButtonPrimary} from './Buttons'
 
 export const cardContainer = styled('div').attrs({
   className: 'flex flex-column ma3 br2 shadow-4'})`
@@ -8,17 +9,21 @@ height: 420px; width: 300px;
 background: linear-gradient(180.1deg, #FFFFFF 0.05%, #E9F7FD 52.48%, #CFF9EF 85.98%);
 `
 
+
+// The Explanation Card is used in the back of assessment cards and concept cards
+
 const titleStyle = styled('div').attrs({className: 'flex f3'})`
-color: ${props => props.theme.tertiary};
+color: ${props => props.theme.primary};
 `
 
 const textStyle = styled('div').attrs({className: 'flex f6'})`
-color: ${props => props.theme.tertiary};
+color: ${props => props.theme.secondary};
 `
-const backButtonStyle = styled('div').attrs({className: 'flex f6'})`
-color: ${props => props.theme.tertiary};
+const backButtonContainer = styled('div').attrs({className: 'flex flex-row justify-end w-100'})`
 `
-
+const explanationFrame = styled('div').attrs({
+  className: 'flex justify-between flex-column w-100 h-100 pa3'
+})``
 
 type ExplanationCardProps = {
   title: string,
@@ -35,10 +40,15 @@ export class ExplanationCard extends Component<ExplanationCardProps, any> {
 	}
 
 	render () {
+		console.log(ButtonPrimary)
 	    return (h(cardContainer,[
-	    	h(titleStyle,this.props.title),
-	    	h(textStyle,this.props.text),
-	    	h(backButtonStyle,{onClick:this.goBack.bind(this)},"Back")
+	    	h(explanationFrame,[
+		    	h(titleStyle,this.props.title),
+		    	h(textStyle,this.props.text),
+		    	h(backButtonContainer,[
+		    		h(ButtonPrimary,{onClick:this.goBack.bind(this),active:false},"Back")
+		    	])
+		    ])
 	    ]))
 	}
 }
